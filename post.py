7dc0b32cf6941f8f8b2c19e116f8c29383ff1aca
Awwab <mawwabkhank2006@gmail.com>
2024-02-13 23:59:16 +0500

    included img url

--- post.py
+++ post.py
@@ -1,6 +1,7 @@
 class Post:
-    def __init__(self, id, title, subtitle, body) -> None:
+    def __init__(self, id, title, subtitle, body, img_url) -> None:
         self.id = id
         self.title = title
         self.subtitle = subtitle
         self.body = body
+        self.img_url = img_url
