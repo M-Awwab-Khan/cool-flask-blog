78968bd2a9c763cdf4601f28f745831662fadd8a
Awwab <mawwabkhank2006@gmail.com>
2024-02-13 14:07:06 +0500

    data fetched from api and dynamically sent to template

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+    posts = response.json()
+    return render_template("index.html", posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
