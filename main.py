f76ff765d9608e7fe45b3711ed1e4928a19c79ae
Awwab <mawwabkhank2006@gmail.com>
2024-02-13 23:48:00 +0500

    routes for about and contact page

--- main.py
+++ main.py
@@ -19,5 +19,13 @@ def show_post(id):
             requested_post = post
     return render_template('post.html', post=requested_post)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
