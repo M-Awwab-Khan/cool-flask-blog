83b2f7af2ad96b626e5c1185a1de2967cc9820a2
Awwab <mawwabkhank2006@gmail.com>
2024-02-13 14:19:03 +0500

    objects instead of dictionaries

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 app = Flask(__name__)
+response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+all_posts = response.json()
+posts = [Post(post['id'], post['title'], post['subtitle'], post['body']) for post in all_posts]
 
 @app.route('/')
 def home():
-    response = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
-    posts = response.json()
     return render_template("index.html", posts=posts)
 
 if __name__ == "__main__":
