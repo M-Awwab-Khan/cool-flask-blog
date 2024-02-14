cb312fe8adfb4160b81b2f0c114601bf3d248da4
Awwab <mawwabkhank2006@gmail.com>
2024-02-14 11:17:51 +0500

    smtp setup + email send on post request

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
 import requests
 from post import Post
+import smtplib
+import os
+
+GMAIL_USER = 'mawwabkhank2006@gmail.com'
+GMAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
 
 app = Flask(__name__)
 response = requests.get('https://api.npoint.io/674f5423f73deab1e9a7')
@@ -26,10 +31,18 @@ def about():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        print(request.form['name'])
-        print(request.form['email'])
-        print(request.form['phone'])
-        print(request.form['message'])
+        msg = f"Subject:New Message\n\nName: {request.form['name']}\nEmail: {request.form['email']}\nPhone: {request.form['phone']}\nMessage: {request.form['message']}"
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(
+                user=GMAIL_USER,
+                password=GMAIL_PASSWORD
+            )
+            connection.sendmail(
+                from_addr=request.form['email'],
+                to_addrs=GMAIL_USER,
+                msg=msg
+            )
         return render_template('contact.html', msg_sent = True)
     else:
         return render_template('contact.html',  msg_sent = False)
