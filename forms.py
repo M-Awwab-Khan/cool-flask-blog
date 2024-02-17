4351c4befc142b87c49f952bb89afd09094caf12
Awwab <mawwabkhank2006@gmail.com>
2024-02-17 17:50:17 +0500

    comment form

--- forms.py
+++ forms.py
@@ -27,3 +27,6 @@ class LoginForm(FlaskForm):
     submit = SubmitField("LOG ME IN")
 
 # TODO: Create a CommentForm so users can leave comments below posts
+class CommentForm(FlaskForm):
+    comment_text = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
