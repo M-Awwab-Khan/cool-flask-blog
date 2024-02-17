4643fc1ef7b8845e1d42520f3beb1c73d350e50d
Awwab <mawwabkhank2006@gmail.com>
2024-02-17 14:01:56 +0500

    RegisterForm structure

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
@@ -14,7 +14,11 @@ class CreatePostForm(FlaskForm):
 
 
 # TODO: Create a RegisterForm to register new users
-
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[Email(check_deliverability=True)])
+    password = StringField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("SIGN ME UP")
 
 # TODO: Create a LoginForm to login existing users
 
